bd2ebdf3805cb093b988345ca1d262e5345d2b14
Hibiii <69405847+Hibiii@users.noreply.github.com>
2020-08-09 03:39:54 -0300

    Add comments

--- src/main/java/com/github/hibiii/waveaway/WaveAway.java
+++ src/main/java/com/github/hibiii/waveaway/WaveAway.java
@@ -5,6 +5,7 @@ import org.lwjgl.glfw.GLFW;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -18,20 +19,25 @@ public class WaveAway implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		// Init the keybinds
 		bindWaveMainHand = new KeyBinding (
-				"key.waveaway.waveMainHand",
-				InputUtil.Type.KEYSYM,
-				GLFW.GLFW_KEY_F7,
-				"key.categories.multiplayer");
+				"key.waveaway.waveMainHand", // Translation key
+				InputUtil.Type.KEYSYM,       // Type
+				GLFW.GLFW_KEY_F7,            // Default
+				"key.categories.multiplayer" // Category translation key
+		);
 		bindWaveOffHand = new KeyBinding (
 				"key.waveaway.waveOffHand",
 				InputUtil.Type.KEYSYM,
 				GLFW.GLFW_KEY_F6,
 				"key.categories.multiplayer");
 		
+		// Register the keybinds for a purpose
 		KeyBindingHelper.registerKeyBinding(bindWaveMainHand);
 		KeyBindingHelper.registerKeyBinding(bindWaveOffHand);
 		
+		// Register callback to check for key presses every client tick
+		// (may be faster than the server's world tick)
 		ClientTickCallback.EVENT.register(client -> {
 			while(bindWaveMainHand.wasPressed())
 				MinecraftClient.getInstance().player.swingHand(Hand.MAIN_HAND);
