bc30723ea3b78c0f2b03f628cc542b234b9b67e2
Hibiii <69405847+Hibiii@users.noreply.github.com>
2020-11-09 22:13:20 -0300

    Added waving off hand when looking at air

---
+++ src/main/java/hibiii/waveaway/mixins/AirRightClickMixin.java
@@ -0,0 +1,30 @@
+package hibiii.waveaway.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.HitResult;
+
+@Mixin(MinecraftClient.class)
+public class AirRightClickMixin {
+
+	@Shadow
+	public ClientPlayerEntity player;
+	
+	@Shadow
+	public HitResult crosshairTarget;
+	
+	@Inject(at = @At("TAIL"), method = "doItemUse()V")
+	private void rightClickAirToSwing(CallbackInfo info) {
+		if (this.crosshairTarget.getType() == HitResult.Type.MISS && this.player.getStackInHand(Hand.OFF_HAND).isEmpty()) {
+			this.player.swingHand(Hand.OFF_HAND);
+			return;
+		}
+	}
+}
